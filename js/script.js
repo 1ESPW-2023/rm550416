918e1cb2a32e33f46e393c62324b4f91f1df13f1
Bruno Bastos <86112284+BrnBastos@users.noreply.github.com>
2023-04-18 20:19:59 -0300

    Collections - SPREAD - Arrays - MAP

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
 
 //Exemplo de Funções
-const divElement = document.getElementById("div-box")
+// const divElement = document.getElementById("div-box")
 
-//divElement.addEventListener("click", ()=>{
-//    console.log("divElement");
-//});
+// //divElement.addEventListener("click", ()=>{
+// //    console.log("divElement");
+// //});
 
-divElement.addEventListener("click", mostraElemento);
+// divElement.addEventListener("click", mostraElemento);
 
-function mostraElemento(){
-    console.log(divElement)
-}
-function soma(){
-    let n1 = 10
-    let n2 = 5
-    console.log(n1 + n2)
-}
+// function mostraElemento(){
+//     console.log(divElement)
+// }
+// function soma(){
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
+// }
+// recuperando elementos do html com query selector
+// const liElements = document.querySelectorAll("li");
+
+// //convertendo NODELIST em array com spread
+// const arrayElements = [...liElements];
+
+// console.log(arrayElements);
+
+const arrayElements = [...document.querySelectorAll("li")];
+
+arrayElements.map((el,key)=>{
+
+    console.log(el.textContent.indexOf("1"))
+
+   if(el.textContent.,indexOf("1") != (-1)) {
+      el.textContent = `[ ${el.textContent} ]`;
+   }
+
+});
 
